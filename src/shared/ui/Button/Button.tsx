0ca377c2c372041c22c58a7acb2f19a1d443822a
freshgoldroses <93222569+freshgoldroses@users.noreply.github.com>
2024-04-14 23:35:27 +0200

    Fixed Button Component

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,15 +1,23 @@
 import cls from './Button.module.scss';
 import {classNames} from "shared/lib/classNames/classNames";
-import {ButtonHTMLAttributes} from "react";
+import {ButtonHTMLAttributes, FC} from "react";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
 	className?: string;
 }
 
-export const Button = ({className}:ButtonProps) => {
+export const Button: FC<ButtonProps> = (props) => {
+	const {
+		children,
+		className,
+		...otherProps
+	} = props
 	return (
-		<button className={classNames(cls.Button, {}, [className])}>
-
+		<button className={
+			classNames(cls.Button, {}, [className])}
+			{...otherProps}
+		>
+			{children}
 		</button>
 	);
 };
