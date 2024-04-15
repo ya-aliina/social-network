36ff90f414ebdf2091d4cb12a7d3ab010258a157
freshgoldroses <93222569+freshgoldroses@users.noreply.github.com>
2024-04-15 23:50:36 +0200

    Fixed Button Component

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -2,14 +2,21 @@ import cls from './Button.module.scss';
 import {classNames} from "shared/lib/classNames/classNames";
 import {ButtonHTMLAttributes, FC} from "react";
 
+export enum ButtonTheme {
+	"CLEAR" = "clear",
+	"BORDERED" = "bordered",
+	"ACCENT" = "accent"
+}
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
 	className?: string;
+	theme?: ButtonTheme;
 }
 
 export const Button: FC<ButtonProps> = (props) => {
 	const {
 		children,
 		className,
+		theme = ButtonTheme["BORDERED"],
 		...otherProps
 	} = props
 	return (
