640e1daec62e94305916970ea5712c920c9afd69
freshgoldroses <93222569+freshgoldroses@users.noreply.github.com>
2024-04-16 23:57:20 +0200

    Added Button Themes

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -9,7 +9,7 @@ declare module '*.scss' {
 declare module "*.png"
 declare module "*.jpg"
 
-declare module 'src/assets/svgr/*.svg' {
+declare module '*.svg' {
 	import React from "react";
 	const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
 	export default SVG;
--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -2,7 +2,7 @@ import cls from './ThemeSwitcher.module.scss';
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider/lib/useTheme";
 import LightIcon from "shared/assets/icons/theme-light.svg"
-import DarkIcon from "shared/assets/icons/theme-dark.svg"
+import {Button, ButtonTheme} from "shared/ui/Button/Button";
 
 interface ThemeSwitcherProps {
 	className?: string;
@@ -13,10 +13,9 @@ export const ThemeSwitcher = ({className}:ThemeSwitcherProps) => {
 
 	return (
 		<div className={classNames(cls.ThemeSwitcher, {}, [className])}>
-
-			<button onClick={toggleTheme}>
+			<Button theme={ButtonTheme.CLEAR} onClick={toggleTheme}>
 				<LightIcon />
-			</button>
+			</Button>
 		</div>
 	);
 };
