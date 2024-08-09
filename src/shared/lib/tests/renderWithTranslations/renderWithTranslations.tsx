3db427e65718a67d547ad500bf49e775e7607d79
freshgoldroses <93222569+freshgoldroses@users.noreply.github.com>
2024-08-10 00:01:32 +0200

    Add visual tool for screenshot tests, added screenshots

--- src/shared/lib/tests/renderWithTranslations/renderWithTranslations.tsx
+++ src/shared/lib/tests/renderWithTranslations/renderWithTranslations.tsx
@@ -4,10 +4,18 @@ import { ReactNode } from 'react';
 import { I18nextProvider } from 'react-i18next';
 import i18nForTests from 'shared/config/i18n/i18nForTests';
 
-export function renderWithTranslation(component: ReactNode) {
+export interface renderWithTranslationsOptions {
+    route?: string;
+}
+
+export function renderWithTranslation(component: ReactNode, options: renderWithTranslationsOptions = {}) {
+    const {
+        route = '/',
+    } = options;
+
     return (
         render(
-            <MemoryRouter>
+            <MemoryRouter initialEntries={[route]}>
                 <I18nextProvider i18n={i18nForTests}>
                     { component }
                 </I18nextProvider>
