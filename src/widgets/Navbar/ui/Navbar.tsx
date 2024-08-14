1d126f2f83a76b1a7dc1e5b6445e3d05e7d20b6f
freshgoldroses <93222569+freshgoldroses@users.noreply.github.com>
2024-08-14 22:57:53 +0200

    Add Modal to Navbar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,8 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-// import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-// import { useTranslation } from 'react-i18next';
+import { useCallback, useState } from 'react';
+import { Modal } from 'shared/ui/Modal/Modal';
+import { t } from 'i18next';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -8,13 +10,27 @@ interface NavbarProps {
 }
 
 export function Navbar({ className }:NavbarProps) {
-    // const { t } = useTranslation();
+    const [isAuthModal, setIsAuthModal] = useState(false);
+
+    const onToggleModal = useCallback(() => {
+        setIsAuthModal((prev) => !prev);
+    }, []);
 
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
-            <div className={cls.links}>
-                /
-            </div>
+            <Button
+                theme={ButtonTheme.OUTLINE}
+                className={cls.links}
+                onClick={onToggleModal}
+            >
+                {t('Войти')}
+            </Button>
+            {/* eslint-disable-next-line i18next/no-literal-string */}
+            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus architecto aspernatur
+                ex praesentium quibusdam, recusandae rerum similique vel voluptatibus voluptatum! Blanditiis
+                dicta ducimus ex minus obcaecati quae quidem sed tempora?
+            </Modal>
         </div>
     );
 }
