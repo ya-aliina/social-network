7739aee135492ce47f4bdbdd37073164cac7e1ea
freshgoldroses <93222569+freshgoldroses@users.noreply.github.com>
2024-09-16 00:36:53 +0200

    Add test for loginSlice

---
+++ src/features/AuthByUserName/model/slice/loginSlice.test.ts
@@ -0,0 +1,23 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { LoginSchema } from 'features/AuthByUserName';
+import { loginActions, loginReducer } from './loginSlice';
+
+describe('test loginSlice', () => {
+    test('setUsername', () => {
+        const state: DeepPartial<LoginSchema> = { username: 'admin' };
+
+        expect(loginReducer(
+            state as LoginSchema,
+            loginActions.setUsername('new admin'),
+        )).toEqual({ username: 'new admin' });
+    });
+
+    test('setPassword', () => {
+        const state: DeepPartial<LoginSchema> = { password: '' };
+
+        expect(loginReducer(
+            state as LoginSchema,
+            loginActions.setPassword('123'),
+        )).toEqual({ password: '123' });
+    });
+});
