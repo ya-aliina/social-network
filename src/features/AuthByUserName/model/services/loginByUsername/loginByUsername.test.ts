2bb74e285fa9c51dea3b40aaf53aca4faccf920b
freshgoldroses <93222569+freshgoldroses@users.noreply.github.com>
2024-09-15 22:33:49 +0200

    Fix bug

--- src/features/AuthByUserName/model/services/loginByUsername/loginByUsername.test.ts
+++ src/features/AuthByUserName/model/services/loginByUsername/loginByUsername.test.ts
@@ -6,7 +6,7 @@ import { loginByUsername } from './loginByUsername';
 
 jest.mock('axios');
 
-const mockedAxios = jest.mocked(axios, true);
+const mockedAxios = jest.mocked(axios, { shallow: false });
 
 describe('loginByUsername', () => {
     let dispatch: Dispatch;
@@ -20,12 +20,15 @@ describe('loginByUsername', () => {
     test('succes login', async () => {
         const userValue = { username: '123', id: '1' };
         mockedAxios.post.mockReturnValue(Promise.resolve({ data: userValue }));
+
         const action = loginByUsername({ username: '123', password: '123' });
         const result = await action(dispatch, getState, undefined);
 
         expect(dispatch).toHaveBeenCalledWith(userActions.setAuthData(userValue));
+        expect(dispatch).toHaveBeenCalledTimes(3);
         expect(mockedAxios.post).toHaveBeenCalled();
         expect(result.meta.requestStatus).toEqual('fulfilled');
+        expect(result.payload).toEqual(userValue);
     });
 
     test('error login', async () => {
@@ -33,7 +36,9 @@ describe('loginByUsername', () => {
         const action = loginByUsername({ username: '123', password: '123' });
         const result = await action(dispatch, getState, undefined);
 
+        expect(dispatch).toHaveBeenCalledTimes(2);
         expect(mockedAxios.post).toHaveBeenCalled();
         expect(result.meta.requestStatus).toEqual('rejected');
+        expect(result.payload).toEqual('Error');
     });
 });
