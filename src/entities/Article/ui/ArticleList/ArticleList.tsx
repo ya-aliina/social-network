e8295a84bd1eb7fc6044790188f223f590e67c77
freshgoldroses <93222569+freshgoldroses@users.noreply.github.com>
2024-11-19 23:46:44 +0100

    Add react virtualized

--- src/entities/Article/ui/ArticleList/ArticleList.tsx
+++ src/entities/Article/ui/ArticleList/ArticleList.tsx
@@ -2,6 +2,8 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { memo } from 'react';
 import { Text, TextSize } from 'shared/ui/Text/Text';
 import { useTranslation } from 'react-i18next';
+import { List, ListRowProps, WindowScroller } from 'react-virtualized';
+import { PAGE_ID } from 'widgets/Page/Page';
 import { ArticleListItemSkeleton } from '../ArticleListItem/ArticleListItemSkeleton';
 import { ArticleListItem } from '../ArticleListItem/ArticleListItem';
 import { Article, ArticleView } from '../../model/types/article';
@@ -23,19 +25,45 @@ const getSkeletons = (view: ArticleView) => new Array(view === ArticleView.TILE 
 export const ArticleList = memo((props:ArticleListProps) => {
     const {
         className,
-        articles,
+        articles = [],
         view = ArticleView.TILE,
         isLoading,
         isNewTab,
     } = props;
-
     const { t } = useTranslation();
 
-    const renderArticle = (article: Article) => (
-        <ArticleListItem isNewTab={isNewTab} className={cls.card} article={article} key={article.id} view={view} />
-    );
+    const isList = view === ArticleView.LIST;
 
-    if (!isLoading && !articles?.length) {
+    const itemsPerRow = isList ? 1 : 4;
+    // const rowCount = isList ? articles.length : Math.ceil(articles.length / itemsPerRow);
+
+    const rowRender = ({
+        index, key, style,
+    }: ListRowProps) => {
+        const items = [];
+        const fromIndex = index * itemsPerRow;
+        const toIndex = Math.min(index + itemsPerRow, articles.length);
+
+        for (let i = fromIndex; i < toIndex; i += 1) {
+            items.push(
+                <ArticleListItem
+                    isNewTab={isNewTab}
+                    className={cls.card}
+                    article={articles[i]}
+                    view={view}
+                    key={articles[i].id}
+                />,
+            );
+        }
+
+        return (
+            <div key={key} style={style}>
+                {items}
+            </div>
+        );
+    };
+
+    if (!isLoading && !articles.length) {
         return (
             <div className={classNames(cls.ArticleList, {}, [className, cls[view]])}>
                 <Text className={TextSize.L} text={t('Статьи не найдены')} />
@@ -44,13 +72,31 @@ export const ArticleList = memo((props:ArticleListProps) => {
     }
 
     return (
-        <div className={classNames(cls.ArticleList, {}, [className, cls[view]])}>
-            {// @ts-expect-error length
-                articles?.length > 0
-                    ? articles?.map(renderArticle)
-                    : null
-            }
-            {isLoading && getSkeletons(view)}
-        </div>
+        <WindowScroller scrollElement={document.getElementById(PAGE_ID) as Element}>
+
+            {({
+                width, height, registerChild, onChildScroll, isScrolling, scrollTop,
+            }) => (
+                <div
+                    // @ts-expect-error 'd'
+                    ref={registerChild}
+                    className={classNames(cls.ArticleList, {}, [className, cls[view]])}
+                >
+                    <List
+                        height={height ?? 700}
+                        rowCount={articles.length}
+                        rowHeight={isList ? 700 : 330}
+                        rowRenderer={rowRender}
+                        width={width ? width - 80 : 700}
+                        autoHeight
+                        onScroll={onChildScroll}
+                        isScrolling={isScrolling}
+                        scrollTop={scrollTop}
+                    />
+                    {isLoading && getSkeletons(view)}
+                </div>
+            )}
+
+        </WindowScroller>
     );
 });
--- src/widgets/Page/Page.tsx
+++ src/widgets/Page/Page.tsx
@@ -18,6 +18,8 @@ interface PageProps {
     onScrollEnd?: () => void;
 }
 
+export const PAGE_ID = 'PAGE_ID';
+
 export const Page = memo(({ className, children, onScrollEnd }:PageProps) => {
     const dispatch = useAppDispatch();
     const triggerRef = useRef() as MutableRefObject<HTMLDivElement>;
@@ -47,6 +49,7 @@ export const Page = memo(({ className, children, onScrollEnd }:PageProps) => {
             ref={wrapperRef}
             className={classNames(cls.Page, {}, [className])}
             onScroll={onScroll}
+            id={PAGE_ID}
         >
             {children}
             {onScrollEnd ? <div className={cls.trigger} ref={triggerRef} /> : null}
